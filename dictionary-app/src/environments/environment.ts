fcdc3749c4839bc93a3a4578ed16bc4131708673
michal <marelix2@gmail.com>
2018-05-22 09:31:51 +0200

    db auth changes

--- dictionary-app/src/environments/environment.ts
+++ dictionary-app/src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAKekL3gJse17ywtdX_-22Rr8Be20aU_QA",
-    authDomain: "diction-app.firebaseapp.com",
-    databaseURL: "https://diction-app.firebaseio.com",
-    projectId: "diction-app",
-    storageBucket: "diction-app.appspot.com",
-    messagingSenderId: "714079482775"
+    apiKey: "AIzaSyD2v_bWTX-B1C7nHrh1y3E0XfvwXyhMg_s",
+    authDomain: "dictionary-app-56c7e.firebaseapp.com",
+    databaseURL: "https://dictionary-app-56c7e.firebaseio.com",
+    projectId: "dictionary-app-56c7e",
+    storageBucket: "dictionary-app-56c7e.appspot.com",
+    messagingSenderId: "518450651384"
   }
 };
